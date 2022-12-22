77aec35bb2c5b45c02347b152ab0466d47528333
Ubuntu <ubuntu@ip-172-31-3-120.ap-northeast-2.compute.internal>
2022-12-22 18:31:09 +0000

    kakao login backend api

--- BE/index.js
+++ BE/index.js
@@ -4,7 +4,7 @@ const port = 8000;
 const cors = require('cors');
 
 app.use(cors());
-
+app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
 const router = require("./routes");
